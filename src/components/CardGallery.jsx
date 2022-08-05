f9ddf1bfb3dbb0a682e84afcdd3a7d393b100643
Baz Murphy <bazmurphy@gmail.com>
2022-08-05 22:08:42 +0100

    minor variable changes

--- src/components/CardGallery.jsx
+++ src/components/CardGallery.jsx
@@ -9,10 +9,10 @@ function CardGallery() {
         <div className="card-gallery-container">
             <Card
                 cardPhoto = {cardPhotoKatie}
-                cardRating = {5.0}
+                cardRating = "5.0"
                 cardReviewCount = {6}
-                cardLocation = {"USA"}
-                cardDescription = {"Life Lessons with Katie Zeferes"}
+                cardLocation = "USA"
+                cardDescription = "Life Lessons with Katie Zeferes"
                 cardPrice = {136}
             />
         </div>
