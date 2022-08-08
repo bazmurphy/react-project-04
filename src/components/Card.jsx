bf6de77ba14c7e637037ad883dc53371fc2b3470
Baz Murphy <bazmurphy@gmail.com>
2022-08-08 11:47:02 +0100

    learning spread operator unpacking of data objects directly into props

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,33 +4,37 @@ import star from "../assets/star.png";
 
 export default function Card(props) {
 
-    let cardOpenSpotsText;
+    // console.log(props)
+    // console.log(props.element)
+    // console.log(props.element.openSpots)
 
-    if (props.cardOpenSpots === 0) {
-        cardOpenSpotsText = "SOLD OUT";
-    } else if (props.cardLocation === "Online") {
-        cardOpenSpotsText = "ONLINE";
+    let openSpotsText;
+
+    if (props.element.openSpots === 0) {
+        openSpotsText = "SOLD OUT";
+    } else if (props.element.location === "Online") {
+        openSpotsText = "ONLINE";
     }
 
     return (
         <section className="card-container">
             <section className="card-photo-container">
-                {cardOpenSpotsText !== undefined && <span className="card-status">{cardOpenSpotsText}</span>}
-                <img className="card-photo" src={`../images/${props.cardPhoto}`} alt="" />
+                {openSpotsText !== undefined && <span className="card-status">{openSpotsText}</span>}
+                <img className="card-photo" src={`../images/${props.element.coverImg}`} alt="" />
             </section>
             <section className="card-info-section-one">
                 <img className="card-star" src={star} alt="" />
-                <span className="card-rating">{props.cardRating}</span>
-                <span className="card-rating-count">({props.cardReviewCount})</span>
+                <span className="card-rating">{props.element.stats.rating}</span>
+                <span className="card-rating-count">({props.element.stats.reviewCount})</span>
                 <span className="card-bullet-point">&bull;</span>
-                <span className="card-location">{props.cardLocation}</span>
+                <span className="card-location">{props.element.location}</span>
             </section>
             <section className="card-info-section-two">
-                <span className="card-description">{props.cardDescription}</span>
+                <span className="card-description">{props.element.title}</span>
             </section>
             <section className="card-info-section-three">
                 <span className="card-from">From</span>
-                <span className="card-price">${props.cardPrice}</span>
+                <span className="card-price">${props.element.price}</span>
                 <span className="card-per-person">/ person</span>
             </section>
         </section>
--- src/components/CardGallery.jsx
+++ src/components/CardGallery.jsx
@@ -9,15 +9,43 @@ export default function CardGallery() {
     // console.log(cardData)
 
     const CardElements = cardData.map(element => {
+        console.log(element)
         return <Card
+
+                // KEY, we need to assign key to a unique id from our data set
                 key = {element.id}
-                cardOpenSpots = {element.openSpots}
-                cardPhoto = {element.coverImg}
-                cardRating = {element.stats.rating}
-                cardReviewCount = {element.stats.reviewCount}
-                cardLocation = {element.location}
-                cardDescription = {element.title}
-                cardPrice = {element.price}
+
+
+                // Third version: 
+
+                // using the spread operator to spread "element" into the Component's props
+                // it takes all the keys in the objects in cardData.js and then unpacks them all for us 
+                // like this:
+                // id = {element.id}
+                // title = {element.title}
+                // etc. etc.
+                
+                // {...element}
+
+                // because its unpacked directly into props...
+                // you then have to go back into the component and remove the sub-object name, props.element.____ becomes props.____
+
+
+                // Second version :
+
+                element = {element}
+
+
+                // First version :
+
+                // cardOpenSpots = {element.openSpots}
+                // cardPhoto = {element.coverImg}
+                // cardRating = {element.stats.rating}
+                // cardReviewCount = {element.stats.reviewCount}
+                // cardLocation = {element.location}
+                // cardDescription = {element.title}
+                // cardPrice = {element.price}
+
                 />
     });
 
