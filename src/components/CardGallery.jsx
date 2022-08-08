38b054b62dbbb605b61982de57b2f7950355f075
Baz Murphy <bazmurphy@gmail.com>
2022-08-08 10:54:11 +0100

    added key with value of id, to the map

--- src/components/CardGallery.jsx
+++ src/components/CardGallery.jsx
@@ -10,6 +10,7 @@ export default function CardGallery() {
 
     const CardElements = cardData.map(element => {
         return <Card
+                key = {element.id}
                 cardAvailableSlots = {element.openSpots > 0 ? element.openSpots: "SOLD OUT"}
                 cardPhoto = {element.coverImg}
                 cardRating = {element.stats.rating}
